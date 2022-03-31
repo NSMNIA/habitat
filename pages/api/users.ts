d31fac79e474862ebd2297918fc8d215b6ce50c0
Kevin Stoop <433243@student.fontys.nl>
2022-03-31 18:50:31 +0200

    first api call added with mysql
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

---
+++ pages/api/users.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next/types';
+import Users from '../../database/models/users';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'post') return res.status(503).json({ success: 0, message: 'Method not allowed' })
+    try {
+        await Users.findAll({
+            attributes: ['firstname', 'lastname', 'email'],
+            limit: 100
+        }).then((found: object) => {
+            if (!found) return res.json({ success: 0, message: 'No users' });
+            return res.json({ success: 1, message: 'Users found', users: found });
+        });
+    } catch (err: any) {
+        return res.json({ success: 0, message: err.message });
+    }
+}
+
+export default handler;
