037921046208366cdf4eb4134a926312d14835df
Kevin Stoop <433243@student.fontys.nl>
2022-05-03 11:09:43 +0200

    filter cities added
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- pages/api/properties/search.ts
+++ pages/api/properties/search.ts
@@ -3,13 +3,17 @@ import { PrismaClient } from '@prisma/client';
 const handler = async (req: any, res: any) => {
     const prisma = new PrismaClient()
     if (req.method !== 'POST') return res.status(503).json({ success: 0, message: 'Method not allowed' });
+    const { search } = req.body;
     try {
-        const cities = await prisma.properties.groupBy({
-            by: ['city']
-        })
-        await prisma.properties.findMany().then(found => {
+        await prisma.properties.findMany({
+            where: {
+                city: {
+                    contains: search.toString()
+                }
+            }
+        }).then(found => {
             if (!found) return res.status(500).json({ success: 0, message: 'Internal server error' });
-            return res.status(200).json({ success: 1, message: 'Properties found successfully', properties: found, cities: cities });
+            return res.status(200).json({ success: 1, message: 'Properties found successfully', data: found });
         })
     } catch (err: any) {
         return res.json({ success: 0, message: err.message });
---
+++ pages/properties/[...search].tsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { GetServerSidePropsContext } from "next";
+import Link from "next/link";
+
+type Props = {
+    search: any;
+}
+
+const Search = ({ search }: Props) => {
+    return (
+        <div>
+            {search?.map((property: any) => {
+                return (
+                    <div key={property.propertyId}>
+                        <Link href={`/property/${property.propertyId}`} >
+                            {`${property.addressTitle} | ${property.city}`}
+                        </Link>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const { search } = context.query;
+
+    const s = await axios.post(`${process.env.NEXTAUTH_URL}/api/properties/search`, {
+        search
+    }).then(found => {
+        return found.data
+    }).catch(err => {
+        return {
+            redirect: {
+                destination: '/properties',
+                permanent: false,
+            },
+            props: {},
+        }
+    });
+
+    if (s.success === 1) {
+        return {
+            props: {
+                search: s.data
+            }
+        }
+    }
+
+    return {
+        redirect: {
+            destination: '/properties',
+            permanent: false,
+        },
+        props: {},
+    }
+}
+
+export default Search
