b09c1265ed1155cd34dbdba50c545e73c63df5ed
Klaas <Klaaspetalo@live.nl>
2022-05-12 09:41:15 +0200

    added footer file

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next/types";
+import styles from '../../styles/Navbar.module.scss';
+
+const Footer: NextPage = () => {
+
+    return (
+        <>
+            <div className={styles['navigation-wrapper']}>
+            </div>
+        </>
+
+    )
+}
+
+export default Footer
