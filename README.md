a18b056ceda81f25e44dbb6825ea7ef252a2c496
Kevin Stoop <433243@student.fontys.nl>
2022-05-26 10:25:18 +0200

    📑 readme updated
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-## Habitat360
+<h1 align="center">Habitat 360</h1>
+
+# The project
+This project is made with [Next.js](https://nextjs.org/). It is a application that is build on [React](https://reactjs.org/) and uses [NextAuth](https://next-auth.js.org/) to provide a seamless user experience.
+
+## NextAuth
+NextAuth is a [Next.js](https://nextjs.org/) based authentication and authorization system. It is built on top of [Passport.js](https://www.passportjs.org/). First, NextAuth is configured to use a [database](https://next-auth.js.org/adapters/prisma) that uses [MySQL](https://www.mysql.com/). The database connection is connected via [Prisma](https://www.prisma.io/).
+
+In the file [/pages/api/auth/[...nextauth].ts](https://github.com/NSMNIA/habitat/blob/main/pages/api/auth/%5B...nextauth%5D.ts) are the authentication providers. For now, it uses the [Google](https://next-auth.js.org/providers/google) and [email](https://next-auth.js.org/providers/email) provider.
+
+The callback gets the user's information from the database and creates a session. This session is stored in a cookie of the browser as a [JSON Web Token](https://jwt.io/).
+
+## Middleware
+To use the authentication system, the user must be authenticated. The authentication middleware is used to check if the user is authenticated or if the user is autorised. The middleware is will be executed before the route handler. You can use the [Next Middleware](https://nextjs.org/docs/advanced-features/middleware) function to create the middleware.
+
+### Example
+To see if the user is autorised to access a route you can use the following code:
+```typescript
+// pages/promoter/_middleware.ts
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: any) {
+    const session: any = await getToken({
+        req,
+        secret: process.env.JWT_SECRET
+    });
+    const { pathname, origin } = req.nextUrl;
+    if (!session) return NextResponse.redirect(`${origin}/`);
+    if (session?.user?.Roles?.role_type.toLowerCase() !== 'promoter') return NextResponse.redirect(`${origin}/`)
+    return NextResponse.next();
+}
+```
+First it will check if the user is authenticated. If the user is not authenticated, the user will be redirected to the homepage. If the user is authenticated, it will check if the user is a promoter. If the user is not a promoter, the user will be redirected to the homepage.
+
+## Database
+The database is connected to the [Prisma](https://www.prisma.io/) database. The database is used to store the users and their information. To use the database, you must import the [Prisma](https://www.prisma.io/) library and the [PrismaClient](https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/). In the file [/prisma/schema.prisma](https://github.com/NSMNIA/habitat/blob/main/prisma/schema.prisma) is the database schema. You can use the [Prisma CLI](https://www.prisma.io/docs/reference/api-reference/command-reference) to generate the schema.
+
+## Environment variables
+To create the enviroment variables, you'll need to create a file called `.env` in the root directory of the project. If the file is not created the application will not be working. The file should contain the following variables:
+```dotenv
+NODE_ENV="development"
+DB_USERNAME=""
+DB_PASSWORD=""
+DB_NAME=""
+DB_HOSTNAME=""
+DB_PORT=""
+DATABASE_URL="mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}";
+EMAIL_SERVER_HOST=""
+EMAIL_SERVER_PORT=""
+EMAIL_SERVER_USER=""
+EMAIL_SERVER_PASSWORD=""
+EMAIL_FROM=""
+NEXTAUTH_URL="http://localhost:3000"
+NEXTAUTH_SECRET=""
+JWT_SECRET=""
+GOOGLE_ID=""
+GOOGLE_SECRET=""
+NEXT_PUBLIC_GOOGLE_MAPS_API_KEY=""
+```
+
+## Getting data
+To get data in the pages, you'll need the function `getServerSideProps`. To get data form the database you'll need to make an `API` route. The `API` needs to be called in the `getServerSideProps` function. The data can be returned as a JSON object and can later be used in the page.
+
+### Example
+```tsx
+// pages/properties/index.tsx
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    ...
+    const p = await axios.post(`${process.env.NEXTAUTH_URL}/api/properties/all`).then(found => {
+        return found.data
+    }).catch(err => {
+        console.log(err);
+    });
+    ...
+}
+```
+```tsx
+// pages/api/properties/all.ts
+const handler = async (req: any, res: any) => {
+    const prisma = new PrismaClient()
+    if (req.method !== 'POST') return res.status(503).json({ success: 0, message: 'Method not allowed' });
+    try {
+        const cities = await prisma.properties.groupBy({
+            by: ['city']
+        })
+        await prisma.properties.findMany({
+            include: {
+                PropertyFiles: true
+            }
+        }).then(found => {
+            if (!found) return res.status(500).json({ success: 0, message: 'Internal server error' });
+            return res.status(200).json({ success: 1, message: 'Properties found successfully', properties: found, cities: cities });
+        })
+    } catch (err: any) {
+        return res.json({ success: 0, message: err.message });
+    }
+}
+```
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,130 +15,130 @@ import styles from '../styles/Home.module.scss'
 
 
 type Props = {
-  properties: any,
-  neighbourhood: any
+    properties: any,
+    neighbourhood: any
 }
 
 const Home = (props: Props) => {
-  const { t } = useTranslation();
-  const { properties, neighbourhood } = props
-  return (
-    <>
-      <Head>
-        <title>Habitat - Home</title>
-      </Head>
-      <Navbar />
-      <main className={styles['home']}>
-        <section>
-          <div className={styles['home_teaser']}>
-            <div className={styles['home_teaser-content']}>
-              <h1 className={styles['home_teaser-content--title']}>{t('Take a')} <span>{t('look')}</span> {t('inside')}</h1>
-              <ArrowRight className={styles['home_teaser-content--arrow']} size={48} />
-            </div>
-            <Image src='/assets/images/home-teaser-enhanced.jpg' alt="home-teaser" layout="fill" objectPosition="center -300px" objectFit="cover" placeholder='blur' blurDataURL='/assets/images/home-teaser.png' />
-          </div>
-        </section>
-        <section className={styles['home_search-section']}>
-          <div className={styles['home_search']}>
-            <h2 className={styles['home_search--title']}>{t('Find a property')}</h2>
-            <div className={styles['home_search--inputs']}>
-              <input className={styles['search-field'] + ' input-text input-type--search'} type="text" placeholder={t('Place, address or zipcode')} />
-              <button className="cta-button--alt">
-                {t('Search')}
-              </button>
-            </div>
-          </div>
-        </section>
-        <section className={styles['home_tours-section']}>
-          <div className={styles['home_tours']}>
-            <div className="title">
-              <h2 className="title--left">{t('Visit our')} <span>{t('3D tours')}</span> </h2>
-              <Link href={'/properties'}>
-                <a className="title--right">{t('Explore All')} <ArrowRight className="title--right-arrow" size={22} /></a>
-              </Link>
-            </div>
-            <div className={styles['home_tours-content']}>
-              <div className={styles['home_tours-content--grid']}>
-                <PropertyCard properties={properties} />
-              </div>
-            </div>
-          </div>
-        </section>
-        {neighbourhood && <>
-          <section className={styles['home_highlighted-section']}>
-            <div className={styles['home_highlighted']}>
-              <p className='title-top'>{t('Highlighted')}</p>
-              <div className="title">
-                <h2 className="title--left">{t('In the')} <span>{t('neighbourhood')}</span> </h2>
-                <Link href={`/property/${neighbourhood?.propertyId}`}>
-                  <a className="title--right">{t('Visit')} <ArrowRight className="title--right-arrow" size={22} /></a>
-                </Link>
-              </div>
-            </div>
-          </section>
-          <section className={styles['home_highlighted-content-section']}>
-            <div className={styles['home_highlighted-content']}>
-              <div className={styles['home_highlighted-content--left']}>
-                {neighbourhood?.PropertyFiles?.[0] ? <Image src={`/assets/uploads/${neighbourhood?.PropertyFiles?.[0]?.fileName}`} blurDataURL={`/assets/uploads/${neighbourhood?.PropertyFiles?.[0]?.fileName}`} alt="home-preview" layout="fill" objectPosition="center center" objectFit="cover" placeholder='blur' /> : ''}
-              </div>
-              <div className={styles['home_highlighted-content--right']}>
-                <h3>{neighbourhood?.type === 'new' ? 'New project' : `House for ${neighbourhood?.type}`} in {neighbourhood?.city}</h3>
-                <div className={styles['home_highlighted-content--right-images']}>
-                  {neighbourhood?.PropertyFiles?.filter((file: any, i: number) => i !== 0).map((file: any, i: number) => {
-                    return <div key={i}>
-                      <Image src={`/assets/uploads/${file.fileName}`} blurDataURL={`/assets/uploads/${file.fileName}`} alt="home-preview" layout="fill" objectPosition="center center" objectFit="cover" placeholder='blur' />
-                    </div>;
-                  })}
-                </div>
-                <div className={styles['home_highlighted-content--right-info']}>
-                  <div className={styles['home_highlighted-content--right-info__icons']}>
-                    <div>
-                      <FontAwesomeIcon icon={faBed} />
-                      <p>{neighbourhood?.rooms} bedrooms</p>
+    const { t } = useTranslation();
+    const { properties, neighbourhood } = props
+    return (
+        <>
+            <Head>
+                <title>Habitat - Home</title>
+            </Head>
+            <Navbar />
+            <main className={styles['home']}>
+                <section>
+                    <div className={styles['home_teaser']}>
+                        <div className={styles['home_teaser-content']}>
+                            <h1 className={styles['home_teaser-content--title']}>{t('Take a')} <span>{t('look')}</span> {t('inside')}</h1>
+                            <ArrowRight className={styles['home_teaser-content--arrow']} size={48} />
+                        </div>
+                        <Image src='/assets/images/home-teaser-enhanced.jpg' alt="home-teaser" layout="fill" objectPosition="center -300px" objectFit="cover" placeholder='blur' blurDataURL='/assets/images/home-teaser.png' />
                     </div>
-                    <div>
-                      <FontAwesomeIcon icon={faBathtub} />
-                      <p>{neighbourhood?.bathrooms} bathrooms</p>
+                </section>
+                <section className={styles['home_search-section']}>
+                    <div className={styles['home_search']}>
+                        <h2 className={styles['home_search--title']}>{t('Find a property')}</h2>
+                        <div className={styles['home_search--inputs']}>
+                            <input className={styles['search-field'] + ' input-text input-type--search'} type="text" placeholder={t('Place, address or zipcode')} />
+                            <button className="cta-button--alt">
+                                {t('Search')}
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                      <FontAwesomeIcon icon={faSquare} />
-                      <p>{neighbourhood?.totalSurface} m2</p>
+                </section>
+                <section className={styles['home_tours-section']}>
+                    <div className={styles['home_tours']}>
+                        <div className="title">
+                            <h2 className="title--left">{t('Visit our')} <span>{t('3D tours')}</span> </h2>
+                            <Link href={'/properties'}>
+                                <a className="title--right">{t('Explore All')} <ArrowRight className="title--right-arrow" size={22} /></a>
+                            </Link>
+                        </div>
+                        <div className={styles['home_tours-content']}>
+                            <div className={styles['home_tours-content--grid']}>
+                                <PropertyCard properties={properties} />
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                      <FontAwesomeIcon icon={faHouse} />
-                      <p>{neighbourhood?.type === 'new' ? 'New project' : `For ${neighbourhood?.type}`}</p>
-                    </div>
-                  </div>
-                  <div className={styles['home_highlighted-content--right-info__text']}>
-                    <p>{neighbourhood?.extras}</p>
-                    <span>${neighbourhood?.price.toLocaleString('en-US', { minimumFractionDigits: 2 })}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </>}
-      </main>
-      <Footer />
-    </>
-  )
+                </section>
+                {neighbourhood && <>
+                    <section className={styles['home_highlighted-section']}>
+                        <div className={styles['home_highlighted']}>
+                            <p className='title-top'>{t('Highlighted')}</p>
+                            <div className="title">
+                                <h2 className="title--left">{t('In the')} <span>{t('neighbourhood')}</span> </h2>
+                                <Link href={`/property/${neighbourhood?.propertyId}`}>
+                                    <a className="title--right">{t('Visit')} <ArrowRight className="title--right-arrow" size={22} /></a>
+                                </Link>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles['home_highlighted-content-section']}>
+                        <div className={styles['home_highlighted-content']}>
+                            <div className={styles['home_highlighted-content--left']}>
+                                {neighbourhood?.PropertyFiles?.[0] ? <Image src={`/assets/uploads/${neighbourhood?.PropertyFiles?.[0]?.fileName}`} blurDataURL={`/assets/uploads/${neighbourhood?.PropertyFiles?.[0]?.fileName}`} alt="home-preview" layout="fill" objectPosition="center center" objectFit="cover" placeholder='blur' /> : ''}
+                            </div>
+                            <div className={styles['home_highlighted-content--right']}>
+                                <h3>{neighbourhood?.type === 'new' ? 'New project' : `House for ${neighbourhood?.type}`} in {neighbourhood?.city}</h3>
+                                <div className={styles['home_highlighted-content--right-images']}>
+                                    {neighbourhood?.PropertyFiles?.filter((file: any, i: number) => i !== 0).map((file: any, i: number) => {
+                                        return <div key={i}>
+                                            <Image src={`/assets/uploads/${file.fileName}`} blurDataURL={`/assets/uploads/${file.fileName}`} alt="home-preview" layout="fill" objectPosition="center center" objectFit="cover" placeholder='blur' />
+                                        </div>;
+                                    })}
+                                </div>
+                                <div className={styles['home_highlighted-content--right-info']}>
+                                    <div className={styles['home_highlighted-content--right-info__icons']}>
+                                        <div>
+                                            <FontAwesomeIcon icon={faBed} />
+                                            <p>{neighbourhood?.rooms} bedrooms</p>
+                                        </div>
+                                        <div>
+                                            <FontAwesomeIcon icon={faBathtub} />
+                                            <p>{neighbourhood?.bathrooms} bathrooms</p>
+                                        </div>
+                                        <div>
+                                            <FontAwesomeIcon icon={faSquare} />
+                                            <p>{neighbourhood?.totalSurface} m2</p>
+                                        </div>
+                                        <div>
+                                            <FontAwesomeIcon icon={faHouse} />
+                                            <p>{neighbourhood?.type === 'new' ? 'New project' : `For ${neighbourhood?.type}`}</p>
+                                        </div>
+                                    </div>
+                                    <div className={styles['home_highlighted-content--right-info__text']}>
+                                        <p>{neighbourhood?.extras}</p>
+                                        <span>${neighbourhood?.price.toLocaleString('en-US', { minimumFractionDigits: 2 })}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </>}
+            </main>
+            <Footer />
+        </>
+    )
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const p = await axios.post(`${process.env.NEXTAUTH_URL}/api/home`, {
-    city: 'Quito'
-  }).then(found => {
-    return found.data
-  }).catch(err => {
-    console.log(err);
-  });
+    const p = await axios.post(`${process.env.NEXTAUTH_URL}/api/home`, {
+        city: 'Quito'
+    }).then(found => {
+        return found.data
+    }).catch(err => {
+        console.log(err);
+    });
 
-  return {
-    props: {
-      properties: p?.properties || null,
-      neighbourhood: p?.neighbourhood || null,
+    return {
+        props: {
+            properties: p?.properties || null,
+            neighbourhood: p?.neighbourhood || null,
+        }
     }
-  }
 }
 
 
