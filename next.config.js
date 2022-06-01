f35c8edd1154a9e2cbe4aa9ef6c478ef87f661ef
Kevin Stoop <433243@student.fontys.nl>
2022-06-01 23:54:05 +0200

    updated config and testing netlify
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    basePath: '',
     images: {
         domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
     },
