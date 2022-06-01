146588e2fc4381e6fb4d275e2d3d2cdb522ab021
Kevin Stoop <433243@student.fontys.nl>
2022-06-01 19:19:07 +0200

    check if build now is working on netlify
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

---
+++ components/SpaceViewer/index.tsx
@@ -0,0 +1,14 @@
+import PanoView from "../PanoView";
+import SpaceViewerAside from "./SpaceViewerAside";
+
+function SpaceViewer() {
+    return (
+        <>
+            <SpaceViewerAside />
+            <PanoView />
+        </>
+    );
+}
+
+
+export default SpaceViewer;
---
+++ pages/spaces/upload.tsx
@@ -0,0 +1,12 @@
+import SpaceViewer from "../../components/SpaceViewer";
+import SpaceLayout from "../../layouts/SpaceLayout";
+
+function Upload() {
+    return (
+        <SpaceLayout>
+            <SpaceViewer />
+        </SpaceLayout>
+    );
+}
+
+export default Upload;
