7127e98738f8a8f06be86607e4fd8e04167fa929
Kevin Stoop <433243@student.fontys.nl>
2022-06-01 19:37:01 +0200

    updated config file
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

---
+++ components/SpaceViewer/SpaceViewerModal.tsx
@@ -0,0 +1,75 @@
+import { useRef, useState } from "react";
+import styles from "./SpaceViewerModal.module.scss";
+
+import { MarkersPlugin } from "photo-sphere-viewer/dist/plugins/markers";
+import { usePano } from "../../context/PanoContext";
+import { useSpaceViewer, useUpdateSpaceViewer } from "../../context/SpaceVIewerContext";
+
+const Modal = ({ title, coordinates, onTitleChange, onClose, children }: any) => {
+    const updateSpaceViewer = useUpdateSpaceViewer() as any;
+    const spaceViewer = useSpaceViewer() as any;
+    const [image, setImage] = useState<any>(null)
+    const uploadInput = useRef<any>(null);
+
+    const pano = usePano();
+    const markers = pano.viewer.getPlugin(MarkersPlugin);
+
+    const onUpdateSpaceViewer = () => {
+        const space = [...spaceViewer, {
+            id: spaceViewer.length,
+            name: title,
+            image: image,
+            coordinates: coordinates
+        }]
+        updateSpaceViewer(space)
+
+        const markerOptions = {
+            id: `${spaceViewer.length}`,
+            longitude: coordinates.longitude,
+            latitude: coordinates.latitude,
+            image: '../img/pin-red.png',
+            width: 32,
+            height: 32,
+            anchor: 'bottom center',
+            tooltip: title,
+            data: {
+                generated: true,
+                deletable: true
+            }
+        }
+        markers.addMarker(markerOptions);
+        onTitleChange("")
+        onClose()
+    }
+
+    const handleChange = (e: any) => onTitleChange(e.target.value)
+
+    const handleUpload = (e: any) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = () => setImage(reader?.result || '');
+        reader.onerror = error => console.log(error);
+    };
+
+    return (
+        <div className={styles.root}>
+            <div className={styles.bg} onClick={onClose}></div>
+            <div className={styles.box}>
+                <div className={styles.header}>
+                    <h2>Add a title</h2>
+                </div>
+                <div className={styles.body}>
+                    <h3>{children}</h3>
+                    <input ref={uploadInput} onChange={handleUpload} type="file" accept="image/jpg" />
+                    <input type="text" value={title} onChange={handleChange} />
+                </div>
+                <div className={styles.footer}>
+                    <button onClick={onClose}>Close</button>
+                    <button onClick={onUpdateSpaceViewer}>Add</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
